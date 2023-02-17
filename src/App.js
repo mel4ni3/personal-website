90e9c69bce689926c5a89e8df74e02a9dacfe9ea
Melanie Ehrlich <melanie6104@icloud.com>
2023-02-17 02:03:11 -0500

    add info, projects, social icons, and resume

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React, { Component } from "react";
+import React, { Component, useState} from "react";
 import ReactDOM from "react-dom/client";
 import './App.css';
+import { SocialIcon } from 'react-social-icons';
 
 class App extends Component {
   render() {
+
     return (
-      <div  className="title">
-      <h1>Melanie Ehrlich</h1>
-      </div>
+      <><div className="title">
+        <h1>Melanie Ehrlich</h1>
+      </div><div className="App">
+      <SocialIcon url="https://twitter.com/creamcakex"  target="_blank" rel="noopener noreferrer" style={{ margin: 20 }}/>
+      <SocialIcon url="https://github.com/mel4ni3"  target="_blank" rel="noopener noreferrer" style={{ margin: 20 }}/>
+      <SocialIcon url="https://instagram.com/melaniie.exe"  target="_blank" rel="noopener noreferrer" style={{ margin: 20 }}/>
+      <SocialIcon url="https://www.linkedin.com/in/melanie-ehrlich/"  target="_blank" rel="noopener noreferrer" style={{ margin: 20 }}/>
+    </div><div className="info">
+          <p>Hi! I am Melanie Ehrlich, a Computer Science student
+            at the University of Central Florida. Some of my interests
+            include drawing, listening to music, and playing video games. Thanks
+            for visiting my site!
+          </p>
+        </div><div className="projects">
+          <h1>Projects</h1>
+          <p>My projects!</p>
+        </div><div className="resume">
+          <h1>Resume</h1>
+         <a href="https://mel4ni3.github.io/assets/Melanie_Resume.pdf">Click here to download resume.</a>
+        </div></>
     );
   }
 }
 
-
 export default App;
-
-/*<div className="App">
-      <h1>Hello World!</h1>
-    </div>*/
 
 /*import logo from './logo.svg';
 import './App.css';
