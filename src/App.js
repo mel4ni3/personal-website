079585f07f9a981b59434016501c2792f612901e
Melanie Ehrlich <71104590+mel4ni3@users.noreply.github.com>
2024-11-27 13:12:56 -0500

    add gradient background and change card layout

--- src/App.js
+++ src/App.js
@@ -1,43 +1,3 @@
-/*import React, { Component } from "react";
-import ReactDOM from "react-dom/client";
-import Particles from "react-tsparticles";
-import './App.css';
-import { SocialIcon } from 'react-social-icons';
-
-class App extends Component {
-  render() {
-    return (
-      <>
-        <div className="title">
-          <h1>Melanie Ehrlich</h1>
-        </div>
-        <div className="App">
-          <SocialIcon url="https://twitter.com/creamcakex" target="_blank" rel="noopener noreferrer" style={{ margin: 20 }} />
-          <SocialIcon url="https://github.com/mel4ni3" target="_blank" rel="noopener noreferrer" style={{ margin: 20 }} />
-          <SocialIcon url="https://instagram.com/melaniie.exe" target="_blank" rel="noopener noreferrer" style={{ margin: 20 }} />
-          <SocialIcon url="https://www.linkedin.com/in/melanie-ehrlich/" target="_blank" rel="noopener noreferrer" style={{ margin: 20 }} />
-        </div>
-        <div className="info">
-          <p>Hi! I am Melanie Ehrlich, a Computer Science student
-            at the University of Central Florida. Some of my interests
-            include drawing, listening to music, and playing video games. Thanks
-            for visiting my site!
-          </p>
-        </div>
-        <div className="projects">
-          <h1>Projects</h1>
-          <p>My projects!</p>
-        </div>
-        <div className="resume">
-        </div>
-      </>
-    );
-  }
-}
-
-export default App;
-*/
-
 import React, { useCallback } from "react";
 import Particles from "react-tsparticles";
 import { loadSlim } from "tsparticles-slim";
@@ -166,36 +126,44 @@ const App = () => {
                     retina_detect: true
                 }}
                 style={{
-                    position: "absolute",
+                    position: "fixed",
                     top: 0,
                     left: 0,
                     width: "100%",
                     height: "100%",
-                    zIndex: -1,
-                    background: "linear-gradient(to right, #ff7e5f, #feb47b)"
+                    zIndex: -1
                 }}
             />
-            <div className="title">
-                <h1>Melanie Ehrlich</h1>
-            </div>
             <div className="App">
+                <div className="title">
+                    <h1>Melanie Ehrlich</h1>
+                </div>
                 <SocialIcon url="https://twitter.com/creamcakex" target="_blank" rel="noopener noreferrer" style={{ margin: 20 }} />
                 <SocialIcon url="https://github.com/mel4ni3" target="_blank" rel="noopener noreferrer" style={{ margin: 20 }} />
                 <SocialIcon url="https://instagram.com/melaniie.exe" target="_blank" rel="noopener noreferrer" style={{ margin: 20 }} />
                 <SocialIcon url="https://www.linkedin.com/in/melanie-ehrlich/" target="_blank" rel="noopener noreferrer" style={{ margin: 20 }} />
-            </div>
-            <div className="info">
-                <p>Hi! I am Melanie Ehrlich, a Computer Science student
-                    at the University of Central Florida. Some of my interests
-                    include drawing, listening to music, and playing video games. Thanks
-                    for visiting my site!
-                </p>
-            </div>
-            <div className="projects">
-                <h1>Projects</h1>
-                <p>My projects!</p>
-            </div>
-            <div className="resume">
+                <div className="info">
+                    <p>Hi! I am Melanie Ehrlich, a Computer Science student
+                        at the University of Central Florida. Some of my interests
+                        include drawing, listening to music, and playing video games. Thanks
+                        for visiting my site!
+                    </p>
+                </div>
+                <div className="projects">
+                    <h1>Projects</h1>
+                    <p>My projects!</p>
+                </div>
+                <div className="resume">
+                    <h1>Resume</h1>
+                    <a href="https://mel4ni3.github.io/assets/Melanie_Resume.pdf">Click here to download resume.</a>
+                </div>
+                <div className="contact">
+                    <h1>Contact</h1>
+                    <p>Feel free to reach out to me via email at <a href="mailto:melanie6104@icloud.com">melanie6104@icloud.com</a>, or through <a href="https://www.linkedin.com/in/melanie-ehrlich/">LinkedIn</a>.</p>
+                </div>
+                <div className="footer">
+                    <p>&copy; Melanie Ehrlich</p>
+                </div>
             </div>
         </>
     );
